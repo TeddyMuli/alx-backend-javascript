d9cc0e0d637872a2697a64d3d8b8c10a16981528
TeddyMuli <tmuli974@gmail.com>
2024-10-09 16:07:33 +0300

    Create type declartions and CRUD file

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,19 @@
+/// <reference path="./crud.d.ts" />
+
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva'
+};
+
+const updatedRow: RowElement = {
+  ...row,
+  age: 23
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
