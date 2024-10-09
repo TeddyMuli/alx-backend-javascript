e46d19bf19f1f50cff8fd4315b4cae0a0ba7066a
TeddyMuli <tmuli974@gmail.com>
2024-10-09 10:45:04 +0300

    Create Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+interface Teacher {
+  readonly firstName: string,
+  readonly fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  lastName: string,
+  location: string,
+  contract: boolean,
+  [key: string]: any,
+}
