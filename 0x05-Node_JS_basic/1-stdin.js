8ba48e4d3bd91ac70be3f17701c37f8b70300f0a
TeddyMuli <tmuli974@gmail.com>
2024-11-25 13:55:29 +0300

    get user input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -12,5 +12,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('readable', () => {
-  process.stdout.write("Closing\n")
+  process.stdout.write("This important software is now closing\n")
 });
