955b3c49a5eaa68f95e207d937e03339e359f88f
TeddyMuli <tmuli974@gmail.com>
2024-11-26 20:16:36 +0300

    read file asynchronously

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,47 @@
+const fs = require('fs/promises');
+
+function countStudents(path) {
+  return fs
+    .readFile(path, 'utf-8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
+
+      if (lines.length <= 1) {
+        console.log('Number of students: 0');
+        return;
+      }
+
+      const headers = lines[0].split(',');
+      const rows = lines.slice(1).filter((line) => line.trim() !== '');
+
+      const numberOfStudents = rows.length;
+      console.log(`Number of students: ${numberOfStudents}`);
+
+      const studentData = {};
+      rows.forEach((row) => {
+        const values = row.split(',');
+
+        const field = values[headers.indexOf('field')];
+        const firstName = values[headers.indexOf('firstname')];
+
+        if (field && firstName) {
+          if (!studentData[field]) {
+            studentData[field] = [];
+          }
+          studentData[field].push(firstName);
+        }
+      });
+
+      Object.keys(studentData).forEach((field) => {
+        const students = studentData[field];
+        console.log(
+          `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`
+        );
+      });
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
