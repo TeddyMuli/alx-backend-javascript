ed6d84c3e8214e58dc0f85da170b8f8aaadbfa5c
TeddyMuli <tmuli974@gmail.com>
2024-11-26 20:24:18 +0300

    fix stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,8 @@
-/**
- * Program to get name from user in the standard input
- * and display the name to the user
- */
-process.stdout.write('Welcome to Holberton School, what is your name?\n')
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-  if (chunk) {
-    process.stdout.write(`Your name is ${chunk}`)
-  }
-});
-
-process.stdin.on('readable', () => {
-  process.stdout.write("This important software is now closing\n")
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
