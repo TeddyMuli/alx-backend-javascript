c9e815f87a428faf5a2695b9a184f1b14d1365fc
TeddyMuli <tmuli974@gmail.com>
2024-11-27 07:13:48 +0300

    fix 4-http

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -9,7 +9,7 @@ app.on('request', (_, res) => {
   const responseText = 'Hello Holberton School!';
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', responseText.length);
-  res.statusCode(200);
+  res.statusCode = 200;
   res.write(Buffer.from(responseText));
 })
 
