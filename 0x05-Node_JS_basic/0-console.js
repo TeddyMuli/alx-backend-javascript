86e34221c5efa01b802754dcd3d46be7aa088120
TeddyMuli <tmuli974@gmail.com>
2024-11-27 07:22:05 +0300

    fix eslint errors

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,7 +2,7 @@
  * Display message is a function to displat text in STDOUT
  */
 const displayMessage = (message) => {
-  console.log(message)
-}
+  console.log(message);
+};
 
 module.exports = displayMessage;
