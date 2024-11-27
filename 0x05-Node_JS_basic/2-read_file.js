5ba62a9ed0f8c8e7c1665aa4d0f66ea7f44fc483
TeddyMuli <tmuli974@gmail.com>
2024-11-27 07:21:16 +0300

    fix eslint errors

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,24 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, "utf-8").trim()
-    const lines = data.split('\n')
+    const data = fs.readFileSync(path, 'utf-8').trim();
+    const lines = data.split('\n');
 
     if (lines.length <= 1) {
-      console.log("Number of students: 0");
+      console.log('Number of students: 0');
       return;
     }
 
     const headers = lines[0].split(',');
-    const rows = lines.slice(1).filter((line) => line.trim != '');
+    const rows = lines.slice(1).filter((line) => line.trim !== '');
 
     const numberOfStudents = rows.length;
     console.log(`Number of students: ${numberOfStudents}`);
 
-    const studentData = {}
+    const studentData = {};
     rows.forEach((row) => {
-      const values = row.split(",");
+      const values = row.split(',');
 
       const field = values[headers.indexOf('field')];
       const firstName = values[headers.indexOf('firstname')];
@@ -34,11 +34,11 @@ function countStudents(path) {
     Object.keys(studentData).forEach((field) => {
       const students = studentData[field];
       console.log(
-        `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`
+        `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`,
       );
-    })
+    });
   } catch (error) {
-    throw new Error("Cannot load the database")
+    throw new Error('Cannot load the database');
   }
 }
 
