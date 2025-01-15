6d6076c990256b635eae825a679b9d56d82bea0f
TeddyMuli <tmuli974@gmail.com>
2025-01-15 13:16:50 +0300

    fix: change name of class

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,4 +1,4 @@
-export default class HolbertonCourse {
+export default class ALXCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
@@ -10,41 +10,41 @@ export default class HolbertonCourse {
       throw new TypeError('Students must be an array of strings');
     }
 
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
-    return this.name;
+    return this._name;
   }
 
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this.name = value;
+    this._name = value;
   }
 
   get length() {
-    return this.length;
+    return this._length;
   }
 
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this.length = value;
+    this._length = value;
   }
 
   get students() {
-    return this.students;
+    return this._students;
   }
 
   set students(value) {
     if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    this.students = value;
+    this._students = value;
   }
 }
